05e42a1c02d18c707ade3e213c527893b23009db
Ayush Dongre <87798106+ayushdongre22@users.noreply.github.com>
2022-07-09 22:57:31 +0530

    Add files via upload

---
+++ bubble sort.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+void sort(int* arr,int n){
+    for(int i=0;i<n-1;i++){
+        for(int j=i+1;j<n;j++){
+            if(*(arr+i)>*(arr+j)){
+            int c=*(arr+i);
+            *(arr+i)=*(arr+j);
+            *(arr+j)=c;}
+        }
+    }
+}
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0; i<n; i++){cin>>arr[i];}
+    sort(arr,n);
+    for(int i=0;i<n;i++){cout<<arr[i]<<endl;}
+    return 0;
+}
